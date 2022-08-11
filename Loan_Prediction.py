d076ffdbeea0f13614145a308c45ec702f88358d
Jonathan Pollyn <j.pollyn@gmail.com>
2022-08-10 22:14:53 -0700

    first repo commit

---
+++ Loan_Prediction.py
@@ -0,0 +1,117 @@
+import pickle
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+import seaborn as sns 
+
+
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+loan_dataset =pd.read_csv(r'loan.csv')
+
+loan_dataset = loan_dataset.dropna()
+
+loan_dataset.replace({"Loan_Status":{'N':0,'Y':1}},inplace=True)
+
+loan_dataset = loan_dataset.replace(to_replace='3+', value=4)
+
+loan_dataset.replace({'Married':{'No':0,'Yes':1},'Gender':{'Male':1,'Female':0},'Self_Employed':{'No':0,'Yes':1},
+                      'Property_Area':{'Rural':0,'Semiurban':1,'Urban':2},'Education':{'Graduate':1,'Not Graduate':0}},inplace=True)
+
+X = loan_dataset[['Gender', 'Married', 'ApplicantIncome', 'LoanAmount', 'Credit_History']]
+y = loan_dataset.Loan_Status
+
+from sklearn.model_selection import train_test_split
+x_train, x_cv, y_train, y_cv = train_test_split(X,y, test_size = 0.2, random_state = 10)
+
+from sklearn.ensemble import RandomForestClassifier 
+model = RandomForestClassifier(max_depth=4, random_state = 10) 
+model.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score
+pred_cv = model.predict(x_cv)
+accuracy_score(y_cv,pred_cv)
+
+pred_train = model.predict(x_train)
+accuracy_score(y_train,pred_train)
+
+pickle_out = open("classifier.pkl", mode = "wb") 
+pickle.dump(model, pickle_out) 
+pickle_out.close()
+
+
+ 
+# loading the trained model
+pickle_in = open('classifier.pkl', 'rb') 
+classifier = pickle.load(pickle_in)
+ 
+@st.cache()
+  
+# defining the function which will make the prediction using the data which the user inputs 
+def prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History):   
+ 
+    # Pre-processing user input    
+    if Gender == "Male":
+        Gender = 0
+    else:
+        Gender = 1
+ 
+    if Married == "Single":
+        Married = 0
+    else:
+        Married = 1
+ 
+    if Credit_History == "Outstanding Debts":
+        Credit_History = 0
+    else:
+        Credit_History = 1  
+
+    # if ApplicantIncome <= 20000.00 & ApplicantIncome >= 250000.00:
+    #     ApplicantIncome = 0
+    # else:
+    #     ApplicantIncome = 1
+ 
+    LoanAmount = LoanAmount / 1000
+ 
+    # Making predictions 
+    prediction = classifier.predict( 
+        [[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]])
+     
+    if prediction == 0:
+        pred = 'Rejected'
+    else:
+        pred = 'Approved'
+    return pred
+      
+  
+# this is the main function that is define the webpage  
+def main():       
+    # front end elements of the web page 
+    html_temp = """ 
+    <div style ="background-color:blue;padding:13px"> 
+    <h1 style ="color:black;text-align:center;">Loan Prediction system</h1> 
+    </div> 
+    """
+      
+    # display the front end aspect
+    st.markdown(html_temp, unsafe_allow_html = True) 
+      
+    # following lines create boxes in which user can enter data required to make prediction 
+    Gender = st.selectbox('Gender',("Male","Female"))
+    Married = st.selectbox('Marital Status',("Single","Married")) 
+    ApplicantIncome = st.number_input("Applicants monthly income") 
+    LoanAmount = st.number_input("Total loan amount")
+    Credit_History = st.selectbox('Credit_History',("Outstanding Debts","No Outstanding Debts"))
+    result =""
+      
+    # when 'Predict' is clicked, make the prediction and store it 
+    if st.button("Predict"): 
+        result = prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History) 
+        st.success('Your loan is {}'.format(result))
+        print(LoanAmount)
+     
+if __name__=='__main__': 
+    main()
